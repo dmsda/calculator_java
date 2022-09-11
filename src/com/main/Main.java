56af1ba98242aa103e5aa9a9987be6486bd81231
Dimas Aditya <dimassuruf66@gmail.com>
2022-09-11 20:24:19 +0700

    function trainning

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -2,84 +2,43 @@ package com.main;
 import java.util.Scanner;
 public class Main {
  public static void main(String[] args) {
-  // Case Menggunakan statement if & else
-  // memasukan nilai user
-//   Scanner inputUser;
-//   float a,b,hasil;
-//   char operator;
-// inputUser = new Scanner(System.in);
-// System.out.println("input nilai a = ");
-// a = inputUser.nextFloat();
-// System.out.println("input operator = ");
-// operator = inputUser.next().charAt(0);
-// System.out.println("input nilai b = ");
-// b = inputUser.nextFloat();
-// System.out.println("Input Rumus Berupa " + a + " " + operator + " " + b);
-
-// //Menentukan nilai user pada operator & rumus
-
-// if ( operator == '+') {
-//   hasil = a + b;
-//  System.out.println("Hasilnya adalah " + hasil);
-  
-// } else if ( operator == '-' ){
-//   hasil = a - b;
-//  System.out.println("Hasilnya adalah " + hasil);
-
-// } else if ( operator == '*' ){
-//   hasil = a * b;
-//  System.out.println("Hasilnya adalah " + hasil);
-
-// } else if ( operator == '/' ){
-// if ( a == 0 ) {
-//   System.out.println("Tidak boleh ada angka 0");
-// } else if ( b == 0 ) {
-//   System.out.println("Tidak boleh ada angka 0");
-// } else {
-//  hasil = a/b;
-//  System.out.println("Hasilnya adalah " + hasil);
-// }
-//  } else {
-//   System.out.println("Operator Tidak ada");
-// }
-      // Menggunakan Statement Switch Case
-
-      Scanner inputUser;
-  float a,b,hasil;
-  String operator;
-inputUser = new Scanner(System.in);
-System.out.println("input nilai a = ");
-a = inputUser.nextFloat();
-System.out.println("input operator = ");
-operator = inputUser.next();
-System.out.println("input nilai b = ");
-b = inputUser.nextFloat();
-System.out.println("Input Rumus Berupa " + a + " " + operator + " " + b);
-
-// Rumus dengan Switch Case
-
-switch (operator) {
-  case "+":
-    hasil = a + b;
-    System.out.println("Hasil" + hasil);
-    break;
-    case "-":
-    hasil = a - b;
-    System.out.println("Hasil" + hasil);
-    break;
-    case "*":
-    hasil = a * b;
-    System.out.println("Hasil" + hasil);
-    break;
-    case "/":
-    hasil = a / b;
-    System.out.println("Hasil" + hasil);
-    break;
-  default:
-  System.out.println("Tidak ada rumusnya maaf");
-    break;
+  try (Scanner inputData = new Scanner(System.in)) {
+    System.out.println("Panjang =");
+      int inputPanjang = inputData.nextInt();
+      System.out.println("Lebar =");
+      int inputLebar = inputData.nextInt();
+      gambar(inputPanjang,inputLebar);
+    
+      System.out.println("Luas =" + luas(inputPanjang,inputLebar));
+      System.out.println("Keliling =" + keliling(inputPanjang,inputLebar));
+    
+      tampilKelilingDanLuas(inputPanjang, inputLebar);
 }
  }
+
+ private static void tampilKelilingDanLuas(int panjang, int lebar) {
+    System.out.println("Luas =" + luas(panjang,lebar));
+  System.out.println("Keliling =" + keliling(panjang,lebar));
+ }
+
+private static int keliling( int panjang, int lebar) {
+    int hasil = (panjang+lebar)*2;
+    return hasil;
+}
+
+ private static int luas(int panjang, int lebar) {
+    int hasil = panjang*lebar;
+    return hasil;
+ }
+
+ private static void gambar(int panjang, int lebar) {
+    for (int i = 0; i < lebar; i++) {
+        for (int j = 0; j < panjang; j++) {
+            System.out.print("* ");
+        }
+        System.out.print("\n");
+    }
+  }
 }
   
 
